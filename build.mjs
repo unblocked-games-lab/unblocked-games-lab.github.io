530aac3b0df18d1c02c4a1b6293f1d1838fef372
Neo Tan <neotan12@hotmail.com>
2026-09-19 19:05:42 -0400

    feat(analytics): integrate Google Analytics GA4 tag G-4NC85924WR

--- build.mjs
+++ build.mjs
@@ -211,6 +211,16 @@ function renderPage({
   
   <link rel="stylesheet" href="${SITE_URL}/style.css">
   
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4NC85924WR"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-4NC85924WR');
+  </script>
+  
   <!-- Structured Data JSON-LD -->
   ${schemaJsonLd
     .map(
